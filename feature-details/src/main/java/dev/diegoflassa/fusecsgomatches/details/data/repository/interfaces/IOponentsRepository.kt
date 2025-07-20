a456d169ce07599f0aca344fd3ee65b60a611504
Diego Felipe Lassa <diegoflassa@gmail.com>
2025-07-20 19:30:28 -0300

    -Removed code comments

--- feature-details/src/main/java/dev/diegoflassa/fusecsgomatches/details/data/repository/interfaces/IOponentsRepository.kt
+++ feature-details/src/main/java/dev/diegoflassa/fusecsgomatches/details/data/repository/interfaces/IOponentsRepository.kt
@@ -3,9 +3,6 @@ package dev.diegoflassa.fusecsgomatches.details.data.repository.interfaces
 import dev.diegoflassa.fusecsgomatches.details.data.dto.OpponentsResponseDto
 import retrofit2.Response
 
-/**
- * Repository interface for operations related to PandaMatchesApiService.
- */
 interface IOpponentsRepository {
 
     suspend fun getOpponents(
--- feature-main/src/main/java/dev/diegoflassa/fusecsgomatches/main/data/repository/interfaces/IMatchesRepository.kt
+++ feature-main/src/main/java/dev/diegoflassa/fusecsgomatches/main/data/repository/interfaces/IMatchesRepository.kt
@@ -3,9 +3,6 @@ package dev.diegoflassa.fusecsgomatches.main.data.repository.interfaces
 import dev.diegoflassa.fusecsgomatches.main.data.dto.MatchDto
 import retrofit2.Response
 
-/**
- * Repository interface for operations related to PandaMatchesApiService.
- */
 interface IMatchesRepository {
 
     suspend fun getMatches(
