32d05a9f93d011ccac44aac3e381429d0e192f01
Diego Felipe Lassa <diegoflassa@gmail.com>
2025-07-23 18:19:52 -0300

    -Removed unused logs in UriSerializer

--- feature-core/src/main/java/dev/diegoflassa/fuzecsgomatches/core/data/serializers/UriSerializer.kt
+++ feature-core/src/main/java/dev/diegoflassa/fuzecsgomatches/core/data/serializers/UriSerializer.kt
@@ -10,15 +10,10 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 object UriSerializer : KSerializer<Uri> {
-
-    private val tag = UriSerializer::class.simpleName
-
     override val descriptor = PrimitiveSerialDescriptor("Uri", PrimitiveKind.STRING)
 
     override fun deserialize(decoder: Decoder): Uri {
         val uriString = decoder.decodeString()
-        Log.i(tag, "deserialize parameter: $uriString")
-
         val ret = when {
             uriString.isEmpty() -> Uri.EMPTY
             else -> uriString.toUri()
