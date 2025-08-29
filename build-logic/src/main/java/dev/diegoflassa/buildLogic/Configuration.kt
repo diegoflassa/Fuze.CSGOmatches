dc2adec1cc3237d781b2743ba38b594044082fd4
Diego Felipe Lassa <diegoflassa@gmail.com>
2025-08-28 23:52:46 -0300

    -Updated app version

--- build-logic/src/main/java/dev/diegoflassa/buildLogic/Configuration.kt
+++ build-logic/src/main/java/dev/diegoflassa/buildLogic/Configuration.kt
@@ -60,7 +60,7 @@ object Configuration {
             if (!initialized) {
                 println("Configuration:: WARNING: VERSION_CODE accessed before initializeBuildCount was called! Returning a default.")
             }
-            return (1015 * 1000) + buildCountValue
+            return (1016 * 1000) + buildCountValue
         }
 
     val VERSION_NAME: String
@@ -68,7 +68,7 @@ object Configuration {
             if (!initialized) {
                 println("Configuration:: WARNING: VERSION_NAME accessed before initializeBuildCount was called! Returning a default.")
             }
-            return "1.0.15-build_$buildCountValue"
+            return "1.0.16-build_$buildCountValue"
         }
 
     fun buildAppName(name: String, versionName: String): String {
